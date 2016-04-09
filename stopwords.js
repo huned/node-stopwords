6c758b6ad694a1f9063f872df747021b75e2aa33
Jean-Elie Barjonet <jebarjonet@users.noreply.github.com>
2016-04-09 18:58:40 +0200

    Update stopwords.js

--- stopwords.js
+++ stopwords.js
@@ -1,3 +1,4 @@
 exports.english = require('./english').english
-exports.spanish = require('./spanish').spanish
+exports.french = require('./french').french
 exports.german = require('./german').german
+exports.spanish = require('./spanish').spanish
